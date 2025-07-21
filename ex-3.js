d95c0a5164fcb8575d0af960c9f42eb2b251c9fd
PTAE43 <kritsada1696@gmail.com>
2025-07-21 14:16:25 +0700

    Done:Assignment : JavaScript Loop Assignment by ptae43

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,14 @@
 // Exercise #3: Find a Minimum Score
-let scores = [100, 20, 3, 1000];
-let minScore;
-// Start coding here
+let studentScores = [100, 20, 3, 1000];
+let minScore = 0;
 
+// Start coding here
+minScore = Math.min(...studentScores);
+// or using a loop
+// let minScore = studentScores[0];
+// for(let score of studentScores) {
+//     if(score < minScore){
+//         minScore = score;
+//     }
+// }
 console.log(minScore);
