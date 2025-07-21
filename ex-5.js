52a216718d8759b2640edafac9ea654c3a424fe6
Preecha <preecha.sayang@gmail.com>
2025-07-21 14:15:52 +0700

    feat: implement exercises 1 to 6 with loop logic

--- ex-5.js
+++ ex-5.js
@@ -1,4 +1,11 @@
 // Exercise #5: Concatenate the Alphabets
 let companyName = "TechUp";
-
+let count=1
 // Start coding here
+for (let letter of companyName){
+    console.log(`Number ${count} character is ${letter}`)
+    count=count+1
+ }
+// for (let i=0; i <companyName.length; i++ ){
+//     console.log(`Number ${i+1} character is ${companyName[i]}`)
+// }
